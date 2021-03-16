ac819dc314dbe1eff7b0b181dcda65ff9c4f6bec
Avinath Gunasekara <avinathgunasekara@gmail.com>
2021-03-16 13:41:49 +0530

    added null safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,7 +27,7 @@ class _HomePageState extends State<HomePage> {
     TimeOfDay(hour: 14, minute: 50),
     TimeOfDay(hour: 15, minute: 20),
   );
-  TimeRangeResult _timeRange;
+  late TimeRangeResult _timeRange;
 
   @override
   void initState() {
@@ -47,14 +47,11 @@ class _HomePageState extends State<HomePage> {
                 padding: const EdgeInsets.only(top: 16, left: leftPadding),
                 child: Text(
                   'Opening Times',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6
-                      .copyWith(fontWeight: FontWeight.bold, color: dark),
                 ),
               ),
               SizedBox(height: 20),
               TimeRange(
+                initialRange: null,
                 fromTitle: Text(
                   'FROM',
                   style: TextStyle(
@@ -90,34 +87,33 @@ class _HomePageState extends State<HomePage> {
                 timeBlock: 30,
                 onRangeCompleted: (range) => setState(() => _timeRange = range),
                 disabledTimeRanges: [
-                  TimeRangeResult(TimeOfDay(hour: 8, minute: 30),
-                      TimeOfDay(hour: 9, minute: 30)),
-                  TimeRangeResult(TimeOfDay(hour: 11, minute: 30),
-                      TimeOfDay(hour: 13, minute: 30))
+                  TimeRangeResult(TimeOfDay(hour: 10, minute: 30),
+                      TimeOfDay(hour: 11, minute: 30)),
+                  TimeRangeResult(TimeOfDay(hour: 13, minute: 30),
+                      TimeOfDay(hour: 14, minute: 30))
                 ],
               ),
               SizedBox(height: 30),
-              if (_timeRange != null)
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, left: leftPadding),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      if (_timeRange.start != null && _timeRange.end != null)
-                        Text(
-                          'Selected Range: ${_timeRange.start.format(context)} - ${_timeRange.end.format(context)}',
-                          style: TextStyle(fontSize: 20, color: dark),
-                        ),
-                      SizedBox(height: 20),
-                      MaterialButton(
-                        child: Text('Default'),
-                        onPressed: () =>
-                            setState(() => _timeRange = _defaultTimeRange),
-                        color: orange,
-                      )
-                    ],
-                  ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, left: leftPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    if (_timeRange.start != null && _timeRange.end != null)
+                      Text(
+                        'Selected Range: ${_timeRange.start!.format(context)} - ${_timeRange.end!.format(context)}',
+                        style: TextStyle(fontSize: 20, color: dark),
+                      ),
+                    SizedBox(height: 20),
+                    MaterialButton(
+                      child: Text('Default'),
+                      onPressed: () =>
+                          setState(() => _timeRange = _defaultTimeRange),
+                      color: orange,
+                    )
+                  ],
                 ),
+              ),
             ],
           ),
         ));
